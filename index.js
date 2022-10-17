3a5c0a2c545c09fb5403035ad30d77a486fb0529
mr-Freeman-S <mr.baranok.slava@gmail.com>
2022-10-17 21:36:08 +0300

    fix

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const bodyParser = require('body-parser')
 
 
 const app = express()
-const port =  8080
+const port =  process.env.PORT || 5000
 const myAccount = {
     user: process.env.LOGIN , 
     pass: process.env.PASSWORD
